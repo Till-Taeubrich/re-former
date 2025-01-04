a24c61bbbd125efef946b6dc3d48d029d4246e2f
Till Taeubrich <till.taeubrich@gmail.com>
2025-01-04 17:29:26 -0500

    migrate new user rows

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_04_211257) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_04_220807) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email"
+    t.string "password"
   end
 end
